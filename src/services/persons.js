67c168d92e4f5ebbb257e311d212f9ee28104634
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-05-13 00:05:09 -0400

    Update persons.js

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,27 +1,27 @@
-import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+import axios from 'axios';
+const baseUrl = 'http://localhost:3001/api/persons';
 
 const getAll = () => {
-  return axios.get(baseUrl).then((response) => response.data);
+	return axios.get(baseUrl).then((response) => response.data);
 };
 
 const createData = (data) => {
-  return axios.post(baseUrl, data).then((response) => response.data);
+	return axios.post(baseUrl, data).then((response) => response.data);
 };
 
 const deleteData = (id) => {
-  return axios.delete(`${baseUrl}/${id}`).then((response) => response.data);
+	return axios.delete(`${baseUrl}/${id}`).then((response) => response.data);
 };
 
 const updateData = (id, newData) => {
-  return axios
-    .put(`${baseUrl}/${id}`, newData)
-    .then((response) => response.data);
+	return axios
+		.put(`${baseUrl}/${id}`, newData)
+		.then((response) => response.data);
 };
 
 export default {
-  getAll,
-  createData,
-  deleteData,
-  updateData,
+	getAll,
+	createData,
+	deleteData,
+	updateData,
 };
